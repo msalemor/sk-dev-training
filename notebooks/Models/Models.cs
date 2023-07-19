565a33e4c93464636ae19da88c71897607d5dd94
Alex Morales <44649358+msalemor@users.noreply.github.com>
2023-07-19 09:04:15 -0400

    Rename file and added supporting code for the future.

---
+++ notebooks/Models/Models.cs
@@ -0,0 +1,35 @@
+using System.Text.Json.Serialization;
+
+namespace Models;
+
+record Message([property: JsonPropertyName("role")] string Role,
+    [property: JsonPropertyName("content")] string Content);
+
+record Prompt([property: JsonPropertyName("messages")] List<Message> Messages,
+    [property: JsonPropertyName("max_tokens")] int Max_tokens,
+    [property: JsonPropertyName("temperature")] double temperature,
+    [property: JsonPropertyName("n")] int N = 1,
+    [property: JsonPropertyName("stop")] string? Stop = null
+    );
+
+record Choice([property: JsonPropertyName("role")] int Index,
+    [property: JsonPropertyName("finish_reason")] string FinishReason,
+    [property: JsonPropertyName("message")] Message Message);
+
+record Usage([property: JsonPropertyName("completion_tokens")] int CompletionTokens,
+    [property: JsonPropertyName("prompt_tokens")] int PromptTokens,
+    [property: JsonPropertyName("total_tokens")] int TotalTokens);
+
+record Completion(
+    [property: JsonPropertyName("role")] string Id,
+    [property: JsonPropertyName("object")] string Object,
+    [property: JsonPropertyName("created")] long Created,
+    [property: JsonPropertyName("model")] string Model,
+    [property: JsonPropertyName("choices")] List<Choice> Choices,
+    [property: JsonPropertyName("usage")] Usage Usage);
+
+enum Role
+{
+    system,
+    user
+}
---
+++ notebooks/Utils/HttpUtils.cs
@@ -0,0 +1,34 @@
+namespace HttpUtil;
+
+using System.Net.Http.Headers;
+
+async Task<Tuple<string?, int, int>> GetGptCompletionAsync(List<Message> history)
+{
+    var prompt = new Prompt(history, 1000, 0.3d);
+    var json = JsonSerializer.Serialize(prompt);
+    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+    // Note: We could add retry logic here
+    var response = await client.PostAsync(new Uri(uri), content);
+
+    if (!response.IsSuccessStatusCode)
+    {
+        Console.WriteLine($"Error: {response.StatusCode}");
+        return new Tuple<string?, int, int>(null, 0, 0);
+    }
+
+    var completion = await response.Content.ReadAsStringAsync();
+    var completionObject = JsonSerializer.Deserialize<Completion>(completion);
+
+    if (completionObject is not null)
+    {
+        var choice = completionObject.Choices[0];
+        if (choice is not null)
+        {
+            return new Tuple<string?, int, int>(choice.Message.Content,
+                completionObject.Usage.CompletionTokens,
+                completionObject.Usage.PromptTokens);
+        }
+    }
+    return new Tuple<string?, int, int>(null, 0, 0);
+}
