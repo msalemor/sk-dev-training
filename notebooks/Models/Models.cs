3e1e062f0ef6cf5d068a45392b7c29856644716e
Alex Morales <alemor@microsoft.com>
2023-07-23 09:50:07 -0400

    Notebook updates.

--- notebooks/Models/Models.cs
+++ notebooks/Models/Models.cs
@@ -1,14 +1,21 @@
 record Message([property: JsonPropertyName("role")] string Role,
     [property: JsonPropertyName("content")] string Content);
 
-record Prompt([property: JsonPropertyName("messages")] List<Message> Messages,
+record GptPrompt([property: JsonPropertyName("messages")] List<Message> Messages,
     [property: JsonPropertyName("max_tokens")] int Max_tokens,
     [property: JsonPropertyName("temperature")] double temperature,
     [property: JsonPropertyName("n")] int N = 1,
     [property: JsonPropertyName("stop")] string? Stop = null
     );
 
-record Choice([property: JsonPropertyName("role")] int Index,
+record DavinciPrompt([property: JsonPropertyName("prompt")] string Prompt,
+    [property: JsonPropertyName("max_tokens")] int Max_tokens,
+    [property: JsonPropertyName("temperature")] double temperature,
+    [property: JsonPropertyName("n")] int N = 1,
+    [property: JsonPropertyName("stop")] string? Stop = null
+    );
+
+record GptChoice([property: JsonPropertyName("role")] int Index,
     [property: JsonPropertyName("finish_reason")] string FinishReason,
     [property: JsonPropertyName("message")] Message Message);
 
@@ -16,13 +23,28 @@ record Usage([property: JsonPropertyName("completion_tokens")] int CompletionTok
     [property: JsonPropertyName("prompt_tokens")] int PromptTokens,
     [property: JsonPropertyName("total_tokens")] int TotalTokens);
 
-record Completion(
+record GptCompletion(
     [property: JsonPropertyName("role")] string Id,
     [property: JsonPropertyName("object")] string Object,
     [property: JsonPropertyName("created")] long Created,
     [property: JsonPropertyName("model")] string Model,
-    [property: JsonPropertyName("choices")] List<Choice> Choices,
+    [property: JsonPropertyName("choices")] List<GptChoice> Choices,
     [property: JsonPropertyName("usage")] Usage Usage);
+
+record DavinciChoice([property: JsonPropertyName("role")] int Index,
+    [property: JsonPropertyName("finish_reason")] string FinishReason,
+    [property: JsonPropertyName("text")] string Text);
+
+record DavinciCompletion(
+    [property: JsonPropertyName("role")] string Id,
+    [property: JsonPropertyName("object")] string Object,
+    [property: JsonPropertyName("created")] long Created,
+    [property: JsonPropertyName("model")] string Model,
+    [property: JsonPropertyName("choices")] List<DavinciChoice> Choices,
+    [property: JsonPropertyName("usage")] Usage Usage);
+
+record Car(string VIN, string Make, string Model, int Year, string Color, string Motor, string Package, int milage, double price);
+record CarTranslation(string VIN, string En, string ES);
 
 enum Role
 {
--- notebooks/Utils/HttpUtils.cs
+++ notebooks/Utils/HttpUtils.cs
@@ -1,7 +1,7 @@
 async Task<Tuple<string?, int, int>> GetGptCompletionAsync(HttpClient client, string uri, List<Message> history)
 {
-    var prompt = new Prompt(history, 1000, 0.3d);
-    var json = JsonSerializer.Serialize(prompt);
+    var payload = new GptPrompt(history, 1000, 0.3d);
+    var json = JsonSerializer.Serialize(payload);
     var content = new StringContent(json, Encoding.UTF8, "application/json");
 
     // Note: We could add retry logic here
@@ -13,10 +13,8 @@ async Task<Tuple<string?, int, int>> GetGptCompletionAsync(HttpClient client, st
         return new Tuple<string?, int, int>(null, 0, 0);
     }
 
-    Console.WriteLine("made it.");
-
     var completion = await response.Content.ReadAsStringAsync();
-    var completionObject = JsonSerializer.Deserialize<Completion>(completion);
+    var completionObject = JsonSerializer.Deserialize<GptCompletion>(completion);
 
     if (completionObject is not null)
     {
@@ -26,6 +24,81 @@ async Task<Tuple<string?, int, int>> GetGptCompletionAsync(HttpClient client, st
             return new Tuple<string?, int, int>(choice.Message.Content,
                 completionObject.Usage.CompletionTokens,
                 completionObject.Usage.PromptTokens);
+        }
+    }
+    return new Tuple<string?, int, int>(null, 0, 0);
+}
+
+HttpContent? CreateJsonContent(object? payload)
+{
+    HttpContent? content = null;
+    if (payload is not null)
+    {
+        byte[] utf8Bytes = payload is string s ?
+            Encoding.UTF8.GetBytes(s) :
+            JsonSerializer.SerializeToUtf8Bytes(payload);
+
+        content = new ByteArrayContent(utf8Bytes);
+        content.Headers.ContentType = new MediaTypeHeaderValue("application/json") { CharSet = "utf-8" };
+    }
+
+    return content;
+}
+
+async Task<Tuple<string?, int, int>> GetDavinciCompletionAsync(HttpClient client, string uri, string apiKey, string prompt)
+{
+    int retries = 0;
+    int delay = 0;
+    var content = CreateJsonContent(new DavinciPrompt(prompt, 1000, 0.3d));
+
+    // Note: We could add retry logic here
+    while (true)
+    {
+        var request = new HttpRequestMessage(HttpMethod.Post, new Uri(uri));
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        request.Headers.Add("api-key", $"{apiKey}");
+        request.Headers.Add("accept", "application/json");
+        //request.Headers.Add("accept-encoding", "gzip, deflate");
+        request.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36 Edg/114.0.1823.86");
+        //request.Headers.Add("x-ms-request-id", $"{gid}");
+        request.Content = content;
+
+        var response = await client.SendAsync(request);
+        if (response.IsSuccessStatusCode)
+        {
+            var completion = await response.Content.ReadAsStringAsync();
+            var completionObject = JsonSerializer.Deserialize<DavinciCompletion>(completion);
+
+            if (completionObject is not null)
+            {
+                var choice = completionObject.Choices[0];
+                if (choice is not null)
+                {
+                    return new Tuple<string?, int, int>(choice.Text,
+                        completionObject.Usage.CompletionTokens,
+                        completionObject.Usage.PromptTokens);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Error: there was not payload from the server.");
+                return new Tuple<string?, int, int>(null, 0, 0);
+            }
+        }
+
+        retries++;
+
+        if (retries == 5)
+        {
+            Console.WriteLine($"Failed after 5 retries. Error: {response.StatusCode} {response.ReasonPhrase}");
+            return new Tuple<string?, int, int>(null, 0, 0);
+        }
+
+        if (response.StatusCode == HttpStatusCode.TooManyRequests)
+        {
+            delay = response.Headers.RetryAfter.Delta?.Seconds ?? 2;
+            Console.WriteLine($"Retry: {retries}. Server requesting a {delay}s delay to try again.\n");
+            await Task.Delay(delay * 1000);
         }
     }
     return new Tuple<string?, int, int>(null, 0, 0);
